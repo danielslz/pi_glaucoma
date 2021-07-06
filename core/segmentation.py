1474b37e502d679f14ce14b7ba0502f48c5fc4ee
Daniel Lima <danielslz@gmail.com>
2021-07-06 17:19:03 -0300

    Fix cup and disc segmentation.

--- core/segmentation.py
+++ core/segmentation.py
@@ -132,11 +132,39 @@ def draw_ellipse_fitting(img, ellipse):
     return output
 
 
-def opening_and_cleaning_area(img):
+def get_big_areas(img):
+    big_areas = 0
+    region = label(img)
+    for props in regionprops(region):
+        # print(props.area)
+        if props.area > 10_000:
+            big_areas += 1
+    return big_areas
+
+
+def remove_nonadjacent_area(img, element_size):
+    output = img.copy()
+    output = opening(output, disk(element_size))
+    output = remove_small_objects(output, 30000)
+
+    return output
+
+
+def opening_and_cleaning_area(img, element_size=15):
     output = img.copy()
 
-    output = opening(output, disk(15))
-    output = remove_small_objects(output, 30000)
+    output = remove_nonadjacent_area(output, element_size)
+    clean_area = get_big_areas(output) > 1
+
+    count = 1
+    while(clean_area):
+        element_size += 10
+        output = remove_nonadjacent_area(output, element_size)
+        clean_area = get_big_areas(output) > 1
+        count += 1
+        if count > 3:
+            break
+    
     output = clear_border(output)
 
     return output
